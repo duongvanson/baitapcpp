0d6f0b5171545fd7020f7f430e379b684f6f4996
Son Duong <duongson.dev@gmail.com>
2020-07-02 07:55:46 +0700

    CT tính tổng 2 số nguyên

---
+++ 02_TinhTong2SoNguyen.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Nhap a, b: ";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "Tong a,b= " << sum;
+    return 0;
+}
